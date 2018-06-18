c291b2cd4af8d6fad2b191698dd4fcbdbdfd8aec
Koen Oosterhof <koenoosterhof@outlook.com>
2018-06-18 15:17:14 +0200

    Added start of inventory

---
+++ Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZuulCS
+{
+    class Inventory {
+        private List<Item> Items = new List<Item>();
+
+        public void Add(Item item) {
+            Items.Add(item);
+        }
+
+        public void Remove(Item item) {
+            Items.Remove(item);
+        }
+
+        public Item GetItem(int index) {
+            return Items[index];
+        }
+    }
+}
